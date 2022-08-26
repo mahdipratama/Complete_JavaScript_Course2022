95d345770750c1c4e9ce3688fe2feec9e2518791
mahdipratama <mahdipratama29@gmail.com>
2022-08-26 17:41:27 +0700

    adding value_and_variable notes

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,19 @@
+let js = 'amazing';
+console.log(40 + 8 + 24 - 10);
+
+console.log('Nami');
+console.log(23);
+
+let firstName = 'Nami';
+console.log(firstName);
+
+let nami_ludo = 'kucing';
+let $function = 29;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
