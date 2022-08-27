45e55b7150ca56e72f6968f67d6c169c8341c4d5
mahdipratama <mahdipratama29@gmail.com>
2022-08-27 16:05:52 +0700

    update assingment

--- 01-Fundamentals-Part-1/Assignment/asignment.js
+++ 01-Fundamentals-Part-1/Assignment/asignment.js
@@ -1,3 +1,7 @@
 let country = 'Indonesia';
 let continent = 'Asian';
 let population = 275000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,4 @@
+/*
 let js = 'amazing';
 console.log(40 + 8 + 24 - 10);
 
@@ -17,3 +18,24 @@ let job1 = 'programmer';
 let job2 = 'teacher';
 
 console.log(myFirstJob);
+*/
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+// console.log(typeof 123);
+// console.log(typeof 'Jonas');
+
+javaScriptIsFun = 'YES';
+console.log(javaScriptIsFun);
+
+let year;
+console.log(year); //Undefined
+console.log(typeof year); //also Undefined
+
+year = 1991; //re-assignment variable (dynamic typing)
+console.log(typeof year); //now, the typeof this variable is Number
+
+console.log(typeof null); //
