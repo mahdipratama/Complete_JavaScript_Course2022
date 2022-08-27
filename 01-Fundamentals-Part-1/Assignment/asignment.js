072568b5b9202e97ffab2693b3e72f5e90b02814
mahdipratama <mahdipratama29@gmail.com>
2022-08-27 16:10:29 +0700

    update assignment

--- 01-Fundamentals-Part-1/Assignment/asignment.js
+++ 01-Fundamentals-Part-1/Assignment/asignment.js
@@ -5,3 +5,10 @@ let population = 275000000;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+let isIsland = true;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
