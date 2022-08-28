859ff5f48e3783fb318502a1a532a0776b6f341f
mahdipratama <mahdipratama29@gmail.com>
2022-08-28 15:59:57 +0700

    finished Coding challenges #1

---
+++ 01-Fundamentals-Part-1/Challenges/challenges.js
@@ -0,0 +1,34 @@
+// TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall
+// Mark
+// let markMass = 78;
+// const markHeight = 1.69;
+// const markBMI = markMass / markHeight ** 2;
+// console.log(`Mark BMI: ${markBMI}`);
+
+// // John
+// let johnMass = 92;
+// const johnHeight = 1.95
+// const johnBMI = johnMass / johnHeight ** 2;
+// console.log(`John BMI: ${johnBMI}`);
+
+// console.log('Is Mark has a higher BMI than John? Answer: ')
+// const markHigherBMI = markBMI > johnBMI;
+// console.log(markHigherBMI);
+
+
+//TEST DATA 2:  Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+//Mark
+let markMass = 95;
+const markHeight = 1.88;
+const markBMI = markMass / markHeight ** 2;
+console.log(`Mark BMI: ${markBMI}`);
+
+// John
+let johnMass = 85;
+const johnHeight = 1.76
+const johnBMI = johnMass / johnHeight ** 2;
+console.log(`John BMI: ${johnBMI}`);
+
+console.log('Is Mark has a higher BMI than John? Answer: ')
+const markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
