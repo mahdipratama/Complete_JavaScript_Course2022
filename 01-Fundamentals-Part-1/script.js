d35dafa95f8fb49bd7522066a57b3f1ba0fa0701
mahdipratama <mahdipratama29@gmail.com>
2022-08-28 14:46:12 +0700

    updating script(notes)

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -58,7 +58,7 @@ const birthYear = 1991;
 
 var job = 'kucing'; //outdated keyword
 job = 'meong';
-*/
+
 
 ////////////////////////////////////
 // Basic Operators
@@ -88,4 +88,21 @@ console.log(x);
 console.log(ageNami > ageLudo); // >, <, >=, <=
 console.log(ageLudo >= 17);
 
-const isFullAge = ageLudo >= 17;
+const isFullAge = ageLudo >= 15;
+console.log(now - 1991 > now - 2018);
+*/
+
+////////////////////////////////////
+// Operator Precedence
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
