c0845ee2e339ff9a2a05fd1c1bee49941a6585e2
mahdipratama <mahdipratama29@gmail.com>
2022-08-29 15:23:48 +0700

    Finished Coding challenges #2

--- 01-Fundamentals-Part-1/Challenges/challenges.js
+++ 01-Fundamentals-Part-1/Challenges/challenges.js
@@ -1,36 +1,48 @@
 // TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall
 // Mark
-// let markMass = 78;
-// const markHeight = 1.69;
-// const markBMI = markMass / markHeight ** 2;
-// console.log(`Mark BMI: ${markBMI}`);
-
-// // John
-// let johnMass = 92;
-// const johnHeight = 1.95
-// const johnBMI = johnMass / johnHeight ** 2;
-// console.log(`John BMI: ${johnBMI}`);
-
-// console.log(`Is Mark has a higher BMI than John? 
-// Answer: `)
-// const markHigherBMI = markBMI > johnBMI;
-// console.log(markHigherBMI);
-
-
-//TEST DATA 2:  Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
-//Mark
-let markMass = 95;
-const markHeight = 1.88;
+let markMass = 78; //78
+const markHeight = 1.69;
 const markBMI = markMass / markHeight ** 2;
-console.log(`Mark BMI: ${markBMI}`);
+const isRataMark = Math.floor(markBMI)
+console.log(`Mark BMI: ${isRataMark}`);
 
 // John
-let johnMass = 85;
-const johnHeight = 1.76
+let johnMass = 92;
+const johnHeight = 1.85
 const johnBMI = johnMass / johnHeight ** 2;
-console.log(`John BMI: ${johnBMI}`);
+const isRataJohn = Math.floor(johnBMI)
+console.log(`John BMI: ${isRataJohn}`);
 
 console.log(`Is Mark has a higher BMI than John? 
 Answer: `)
-const markHigherBMI = markBMI > johnBMI;
-console.log(markHigherBMI);
+const higherBMI = isRataMark > isRataJohn;
+
+//TEST DATA 2:  Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+//Mark
+// let markMass = 95; //78
+// const markHeight = 1.88; //1.69
+// const markBMI = markMass / markHeight ** 2;
+// const isRataMark = Math.floor(markBMI)
+// console.log(`Mark BMI: ${isRataMark}`);
+
+// // John
+// let johnMass = 85; //92
+// const johnHeight = 1.76 //1.85
+// const johnBMI = johnMass / johnHeight ** 2;
+// const isRataJohn = Math.floor(johnBMI)
+// console.log(`John BMI: ${isRataJohn}`);
+
+// console.log(`Is Mark has a higher BMI than John? 
+// Answer: `)
+// const higherBMI = isRataMark > isRataJohn;
+// console.log(higherBMI);
+
+// let isRata = [markBMI, johnBMI];
+// console.log(isRata);
+// console.log(bulat);
+
+if (markBMI > johnBMI) {
+  console.log(`Mark's BMI (${isRataMark}) is higher than John's BMI (${isRataJohn})`);
+} else {
+  console.log(`John's BMI (${isRataJohn}) is higher than Mark's (${isRataMark})`);
+};
