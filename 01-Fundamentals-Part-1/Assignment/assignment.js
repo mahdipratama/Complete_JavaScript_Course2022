c8fc276ced026400ba69931de6a89e2f4b89bf7d
Mahdi Pratama <mahdipratama29@gmail.com>
2022-09-06 15:47:34 +0700

    finish assignment The Switch Statement lecture

--- 01-Fundamentals-Part-1/Assignment/assignment.js
+++ 01-Fundamentals-Part-1/Assignment/assignment.js
@@ -59,3 +59,25 @@ if (language === 'english' && population < 50 && !isIsland) {
 } else {
   console.log(`${country} does not meet your criteria`);
 }
+
+// Lecture: The Switch Statement 
+
+switch (language) {
+  case 'mandarin':
+    console.log('MOST number of native speakers!')
+    break;
+  case 'spanish':
+    console.log('2nd place in number of native speakers')
+    break;
+  case 'english':
+    console.log('3nd place')
+    break;
+  case 'hindi':
+    console.log('Number 4')
+    break;
+  case 'arabic':
+    console.log('5th most spoken language')
+    break;
+  default:
+    console.log('Great language too :D');
+}
