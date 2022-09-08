60c442ca3aa21145b9395fb1def7385c3f164520
Mahdi Pratama <mahdipratama29@gmail.com>
2022-09-08 18:28:00 +0700

    update script note function declaration and expression

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,27 +1,28 @@
 'use strict'
+/*
+let hasDriversLicense = false;
+const passTest = true;
 
-// let hasDriversLicense = false;
-// const passTest = true;
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive');
+console.log(hasDriversLicense)
 
-// if (passTest) hasDriversLicense = true;
-// if (hasDriversLicense) console.log('I can drive');
-// console.log(hasDriversLicense)
-
-// // this reserved word below will cause error silently if we not use strict Mode
-// const interface = 'Audio'; 
-// const private = '333'
+// this reserved word below will cause error silently if we not use strict Mode
+const interface = 'Audio'; 
+const private = '333'
 
 
 
 /////////////////
 // Function
 /////////////////
-// function logger() {
-//   console.log('My name is Nami');
-// }
 
-// Calling/running/invoking the function
-// logger();
+function logger() {
+  console.log('My name is Nami');
+}
+
+Calling/running/invoking the function
+logger();
 
 function fruitProcessor(apples, oranges) {
   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
@@ -34,3 +35,24 @@ console.log(ourJuice);
 
 const ourJuice2 = fruitProcessor(2, 4);
 console.log(ourJuice2);
+*/
+
+
+/////////////////
+// Function Declarations vs Expressions
+/////////////////
+
+//Function declaration 
+function calAge1(birthYear) {
+  return 2022 - birthYear
+}
+const age1 = calAge1(1991);
+
+//Function Expression 
+const calAge2 = function (birthYear) {
+  return 2022 - birthYear
+}
+const age2 = calAge2(1998);
+
+console.log(age1);
+console.log(age2);
