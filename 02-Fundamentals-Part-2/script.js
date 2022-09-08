b0d4ea0d3be422b3b16c2b0b327b8098ce7ea09c
Mahdi Pratama <mahdipratama29@gmail.com>
2022-09-08 16:20:00 +0700

    update script note Function

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,12 +1,36 @@
 'use strict'
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log('I can drive');
-console.log(hasDriversLicense)
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log('I can drive');
+// console.log(hasDriversLicense)
 
-// // this reserved below will cause error silently if we not use strict Mode
-// const interface = 'Audio';
+// // this reserved word below will cause error silently if we not use strict Mode
+// const interface = 'Audio'; 
 // const private = '333'
+
+
+
+/////////////////
+// Function
+/////////////////
+// function logger() {
+//   console.log('My name is Nami');
+// }
+
+// Calling/running/invoking the function
+// logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+// Saving(capture) that return value in to a variable 
+const ourJuice = fruitProcessor(5, 0);
+console.log(ourJuice);
+
+const ourJuice2 = fruitProcessor(2, 4);
+console.log(ourJuice2);
