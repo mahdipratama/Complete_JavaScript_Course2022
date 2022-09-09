0d8c595d4fad97ebd43a9319b2b72fef9cf164f2
Mahdi Pratama <mahdipratama29@gmail.com>
2022-09-09 16:14:49 +0700

    update script note Arrow Function

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -21,7 +21,7 @@ function logger() {
   console.log('My name is Nami');
 }
 
-Calling/running/invoking the function
+//Calling/running/invoking the function
 logger();
 
 function fruitProcessor(apples, oranges) {
@@ -35,12 +35,12 @@ console.log(ourJuice);
 
 const ourJuice2 = fruitProcessor(2, 4);
 console.log(ourJuice2);
-*/
 
 
-/////////////////
+
+////////////////////
 // Function Declarations vs Expressions
-/////////////////
+////////////////////
 
 //Function declaration 
 function calAge1(birthYear) {
@@ -56,3 +56,26 @@ const age2 = calAge2(1998);
 
 console.log(age1);
 console.log(age2);
+*/
+
+
+
+////////////////////
+// Arrow Functions 
+////////////////////
+
+// One-Liner Arrow Function 
+const calcAge3 = birthYear => 2022 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+// Multiple-Liner function 
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2022 - birthYear;
+  const retirement = 65 - age;
+  //return retirement;
+  return `${firstName} retires in ${retirement} years`
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1998, 'Mahdi'));
