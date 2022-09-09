78c1819f9c74122c3ca7895f3ef995cc2f642315
Mahdi Pratama <mahdipratama29@gmail.com>
2022-09-09 15:40:10 +0700

    finish assignment Function Declaration vs. Expressions

--- 02-Fundamentals-Part-2/Assignment/assignment.js
+++ 02-Fundamentals-Part-2/Assignment/assignment.js
@@ -12,3 +12,45 @@ console.log(isNation2);
 
 const isNation3 = describeCountry('United Kingdom', 67, 'London');
 console.log(isNation3);
+
+
+
+// LECTURE: Function Declaration vs. Expressions
+
+// Declaration
+/*
+function percentageOfWorld1(population) {
+  let country = population / 7900 * 100;
+  const percentageCountry = Math.floor(country)
+  return (percentageCountry)
+}
+
+const indonesiaPopulation = [275, percentageOfWorld1(275)];
+console.log(`Indonesia has ${indonesiaPopulation[0]} million people, so it's about ${indonesiaPopulation[1]}% of the world population`);
+
+
+const indiaPopulation = [1380, percentageOfWorld1(1380)];
+console.log(`India has ${indiaPopulation[0]} million people, so it's about ${indiaPopulation[1]}% of the world population`);
+
+
+const chinaPopulation = [1402, percentageOfWorld1(1402)];
+console.log(`China has ${chinaPopulation[0]} million people, so it's about ${chinaPopulation[1]}% of the world population`);
+*/
+
+// Expression
+const percentageOfWorld1 = function (population) {
+  let country = population / 7900 * 100;
+  const percentageCountry = Math.floor(country)
+  return (percentageCountry)
+}
+
+const indonesiaPopulation = [275, percentageOfWorld1(275)];
+console.log(`Indonesia has ${indonesiaPopulation[0]} million people, so it's about ${indonesiaPopulation[1]}% of the world population`);
+
+
+const indiaPopulation = [1380, percentageOfWorld1(1380)];
+console.log(`India has ${indiaPopulation[0]} million people, so it's about ${indiaPopulation[1]}% of the world population`);
+
+
+const chinaPopulation = [1402, percentageOfWorld1(1402)];
+console.log(`China has ${chinaPopulation[0]} million people, so it's about ${chinaPopulation[1]}% of the world population`);
