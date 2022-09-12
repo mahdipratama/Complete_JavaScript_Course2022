dbd4330a27f42822f998591543107cb9f489b98a
Mahdi Pratama <mahdipratama29@gmail.com>
2022-09-12 16:30:52 +0700

    update script note Introduction to Arrays

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -125,3 +125,45 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 console.log(yearsUntilRetirement(1991, 'Jonas'));
 console.log(yearsUntilRetirement(1998, 'Mahdi'));
 console.log(yearsUntilRetirement(1950, 'John'));
+
+
+/////////////////////
+// Introduction to Arrays
+/////////////////////
+const friend1 = 'Michael';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+// friends = ['Bob', 'Alice']
+
+const firstName = 'Jonas';
+const jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+console.log(jonas);
+console.log(jonas.length);
+
+// Exercise
+const umur = function (birthYeah) {
+  return 2022 - birthYeah;
+}
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = umur(years[0]);
+const age2 = umur(years[1]);
+const age3 = umur(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [umur(years[0]), umur(years[1]), umur(years[years.length - 1])];
+console.log(ages);
