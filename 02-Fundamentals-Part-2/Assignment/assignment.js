166d652df61cff380e1effdfae64281a97158bad
Mahdi Pratama <mahdipratama29@gmail.com>
2022-09-15 11:43:42 +0700

    finish assignment Dot vs. Bracker Notation

--- 02-Fundamentals-Part-2/Assignment/assignment.js
+++ 02-Fundamentals-Part-2/Assignment/assignment.js
@@ -119,4 +119,13 @@ const myCountry = {
   language: 'Bahasa',
   population: 275,
   neighbours: ['Malaysia', 'Singapore', 'Australia']
-} 
+}
+
+// LECTURE: Dot vs. Bracket Notation 
+console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}`);
+
+myCountry.population += 2
+console.log(myCountry.population);
+
+myCountry['population'] -= 2;
+console.log(myCountry['population']);
