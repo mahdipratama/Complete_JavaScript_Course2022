5a973f70ff4550c3b8a449821f61f20b563ec345
Mahdi Pratama <mahdipratama29@gmail.com>
2022-09-27 10:04:29 +0700

    add Handling Click Events to script

--- 04-Guess-My-Number/script.js
+++ 04-Guess-My-Number/script.js
@@ -1,11 +1,25 @@
 'use strict';
 
+/*
+// Selecting and Manipulating Elements
 
 console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number!';
+document.querySelector('.message').textContent = '🏆 Correct Number!';
 
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value = 23);
+*/
+
+
+// Handling Click Event
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '📛 Insert a Number!'
+  }
+})
