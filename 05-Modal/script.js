aab47467e1590d2239a9aa5c3306dc206a287641
Mahdi Pratama <mahdipratama29@gmail.com>
2022-09-28 13:39:39 +0700

    add show and close modal features

--- 05-Modal/script.js
+++ 05-Modal/script.js
@@ -4,8 +4,22 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
-console.log(btnsShowModal);
+
+const openModal = function () {
+  console.log('terKlik');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 for (let btns of btnsShowModal) {
-  console.log(btns.textContent);
+  btns.addEventListener('click', openModal);
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
