de5ed99b3cf1b1219ecdeaa41c703bb092c02c8b
Mahdi Pratama <mahdipratama29@gmail.com>
2022-10-08 16:34:56 +0700

    update script note Destructuring Objects lecture

--- 08-Data-Structures-Operators/script.js
+++ 08-Data-Structures-Operators/script.js
@@ -12,11 +12,78 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 22,
+    },
+    sat: {
+      open: 0, // open 24 hours
+      close: 24,
+    },
+  },
+
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  }
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 };
 
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Jl.Bintan',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: 'Saint petersburg, 09',
+  starterIndex: 0,
+});
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// Default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// Mutating variables
+let a = 112;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+
+({ a, b } = obj);
+console.log(a, b);
+
+// Nested Object
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+
+/* 
 //======================//
 // Destructuring Arrays // 
 //======================//
@@ -69,4 +136,4 @@ console.log(i, j, k); // 2, 5, 6
 // Default values 
 const [p = 1, q = 1, r = 1,] = [8, 9];
 console.log(p, q, r); // 8, 9, 1
-
+*/
