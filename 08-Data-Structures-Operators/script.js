df125f9493901ceaf6bf92680c895c49690423af
Mahdi Pratama <mahdipratama29@gmail.com>
2022-10-14 11:37:02 +0700

    update script note Optional Chaining (?.) lecture

--- 08-Data-Structures-Operators/script.js
+++ 08-Data-Structures-Operators/script.js
@@ -4,6 +4,9 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+//======================//
+// Enhanced Object Literals //
+//======================//
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 const openingHours = {
   [weekdays[3]]: {
@@ -58,7 +61,42 @@ const restaurant = {
   },
 };
 
+//======================//
+// Optional Chaining (?.)//
+//======================//
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
 
+// WITH optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example 
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed'
+  console.log(`On ${day} we open at ${open}`);
+}
+
+// Methods 
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+// Arrays 
+const users = [
+  {
+    name: 'jonas ',
+    email: 'hello@jonas.com'
+  }
+];
+
+console.log(users[0]?.name ?? 'User Array empty');
+
+// without ?. 
+if (users.length > 0) console.log(users[0].name); else console.log('User array empty');
+
+/* 
 //======================//
 // Looping Arrays: The for-of Loop //
 //======================//
@@ -75,7 +113,7 @@ for (const [i, el] of menu.entries()) {
 // console.log(([...menu.entries()]));
 
 
-/* 
+
 //======================//
 // Logical Assignment Operators //
 //======================//
