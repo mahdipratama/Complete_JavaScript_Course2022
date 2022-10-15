598fa3013f3b2e02f9513cd434178ae4d630b937
Mahdi Pratama <mahdipratama29@gmail.com>
2022-10-15 15:02:17 +0700

    udpate script note Maps: Fundamentals lecture

--- 08-Data-Structures-Operators/script.js
+++ 08-Data-Structures-Operators/script.js
@@ -61,7 +61,42 @@ const restaurant = {
   },
 };
 
+//======================//
+// Maps: Fundamentals//
+//======================//
 
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy')
+rest.set(2, 'Lisbon, Portugal')
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'We are closed');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 12;
+
+console.log(rest.get(time >= rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set('Heading', document.querySelector('h1'));
+console.log(rest);
+console.log(rest.size);
+
+
+/* 
 //======================//
 // Sets//
 //======================//
@@ -100,7 +135,8 @@ console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).s
 console.log(new Set('NamiSaputri').size);
 
 
-/* 
+
+
 //======================//
 // Looping Objects: Objects Keys, Values, and Entries//
 //======================//
