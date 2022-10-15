dcd56766f011f1ad2306089fa180cd8ff43fe6aa
Mahdi Pratama <mahdipratama29@gmail.com>
2022-10-15 16:02:05 +0700

    update script note Map Iterarion lecture

--- 08-Data-Structures-Operators/script.js
+++ 08-Data-Structures-Operators/script.js
@@ -62,6 +62,50 @@ const restaurant = {
 };
 
 //======================//
+// Maps: Iteration//
+//======================//
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C#'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'correct yey!'],
+  [false, 'Try again!'],
+]);
+console.log(question);
+
+
+// Convert object to Map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+
+// Quiz app
+console.log(question.get('question'));
+for (let [key, value] of question) {
+  if (typeof key === 'number') {
+    console.log(`Answer ${key}: ${value}`);
+  }
+};
+
+// const answer = Number(prompt('Your answer is ?'));
+const answer = 3
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+// Convert Map to Array
+console.log([...question]);
+// console.log(...question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
+
+
+/* 
+//======================//
 // Maps: Fundamentals//
 //======================//
 
@@ -96,7 +140,7 @@ console.log(rest);
 console.log(rest.size);
 
 
-/* 
+
 //======================//
 // Sets//
 //======================//
