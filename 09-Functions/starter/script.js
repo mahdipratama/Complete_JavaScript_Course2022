9c634aa1deeeca7be3960e720bb6cd82be707643
mahdipratama <mahdipratama29@gmail.com>
2023-01-09 11:18:53 +0700

    udpate script note How Passing Arguments Works: Value vs. Reference lecture

--- 09-Functions/starter/script.js
+++ 09-Functions/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/* 
 ///////////////////////////////
 // Default Parameters
 
@@ -44,3 +44,55 @@ createBooking('LH123', undefined, 5);
 
 
 // ===================BORDER====================== //
+
+
+
+
+*/
+///////////////////////////////
+// How Passing Arguments Works: Value vs. Reference 
+
+
+const flight = 'LH123';
+const nami = {
+  name: 'Nami Tsunami',
+  passport: 12213145443,
+}
+
+const checkIn = function (flightNum, passenger) {
+  // this flightNum completely different variable, not got reflected.
+  flightNum = 'LH999';
+
+  // this will reflected, its the same object in memory heap
+  passenger.name = 'Mrs.' + passenger.name;
+
+  if (passenger.passport === 12213145443) {
+    console.log('Checked in')
+  } else {
+    console.log('Wrong passport!')
+  }
+}
+
+checkIn(flight, nami); // outputs: Check in
+console.log(flight);   // outputs:  LH123
+console.log(nami);
+// outputs: { name: 'Mrs.Nami Tsunami', passport: 12213145443 }
+
+// Passing a primitive types to a function is really just the same as creating a copy like this, outside of a function. the value is simply copied.
+const flightNum = flight;
+
+// On the other hand, when we pass an object to a function it's really just like copying an object like this. whatever we change in a copy will also happen in the original.
+const passenger = nami;
+
+
+// this newPassport function below manipulates that person object, it also gets reflected in Nami object.
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000)
+}
+
+// two functions manipulating the same object, causes problem
+newPassport(nami);
+checkIn(flight, nami)  // outputs: Wrong Passport!
+
+
+// There are two terms that are used when dealing with function, which is passing by value, and passing by reference. So javascript does not have passing by reference, only passing by value ! JavaScript does not have pass by reference. For objects in fact we pass in a reference (the memory address of the object) that reference itself is still a value, it's simply a value that contains a memory address. JavaScript pass a reference to the function, but do not pass by reference. pass by reference? which means that the function receives a reference to the memory location of the variable. this allows function to modify the value of the variable.  
